7a686174b44e4773cf00b59679078acdd658c740
HeyItsDrip <ethaneastwood@dal.ca>
2024-12-17 14:00:13 -0400

    Day 15
    Changed locations to complex numbers, unsure if it is more readable because of all the int() calls

--- 2024/15.py
+++ 2024/15.py
@@ -9,16 +9,16 @@ f.close()
 
 warehouse = []
 movements = ''
-robot = (-1, -1)
+robot = 0
 for i in range(len(data)):
     if data[i] and data[i][0] == '#':
         warehouse.append(list(data[i]))
         if '@' in data[i]:
-            robot = (i, data[i].index('@'))
+            robot = i + data[i].index('@') * 1j
     elif data[i]:
         movements += data[i]
         
-new_robot = (robot[0], 2 * robot[1])
+new_robot = robot.real + 2 * robot.imag * 1j
 new_warehouse = []
 for r in range(len(warehouse)):
     new_warehouse.append([])
@@ -30,24 +30,24 @@ for r in range(len(warehouse)):
         else:
             new_warehouse[r] += warehouse[r][c] * 2
         
-dirs = {'^':(-1,0), '>':(0,1), 'v':(1,0), '<':(0,-1)}
+dirs = {'^':-1, '>':1j, 'v':1, '<':-1j}
 for m in movements:
-    next_pos = (robot[0] + dirs[m][0], robot[1] + dirs[m][1])
-    if warehouse[next_pos[0]][next_pos[1]] == '#':
+    next_pos = robot + dirs[m]
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '#':
         continue
-    if warehouse[next_pos[0]][next_pos[1]] == '.':
-        warehouse[robot[0]][robot[1]] = '.'
-        warehouse[next_pos[0]][next_pos[1]] = '@'
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '.':
+        warehouse[int(robot.real)][int(robot.imag)] = '.'
+        warehouse[int(next_pos.real)][int(next_pos.imag)] = '@'
         robot = next_pos
     else:
         first_pos = next_pos
-        while warehouse[next_pos[0]][next_pos[1]] == 'O':
-            next_pos = (next_pos[0] + dirs[m][0], next_pos[1] + dirs[m][1])
+        while warehouse[int(next_pos.real)][int(next_pos.imag)] == 'O':
+            next_pos += dirs[m]
             
-        if warehouse[next_pos[0]][next_pos[1]] == '.':
-            warehouse[next_pos[0]][next_pos[1]] = 'O'
-            warehouse[first_pos[0]][first_pos[1]] = '@'
-            warehouse[robot[0]][robot[1]] = '.'
+        if warehouse[int(next_pos.real)][int(next_pos.imag)] == '.':
+            warehouse[int(next_pos.real)][int(next_pos.imag)] = 'O'
+            warehouse[int(first_pos.real)][int(first_pos.imag)] = '@'
+            warehouse[int(robot.real)][int(robot.imag)] = '.'
             robot = first_pos
 
 GPS = 0
@@ -59,25 +59,26 @@ print("Part 1:", GPS)
 
 def canPush(warehouse, next_pos, m):
     
-    if warehouse[next_pos[0]][next_pos[1]] == '[':
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '[':
         left_side = next_pos
-        right_side = (next_pos[0], next_pos[1] + 1)
-    elif warehouse[next_pos[0]][next_pos[1]] == ']':
+        right_side = next_pos + 1j
+    elif warehouse[int(next_pos.real)][int(next_pos.imag)] == ']':
         right_side = next_pos
-        left_side = (next_pos[0], next_pos[1] - 1)
+        left_side = next_pos - 1j
     else:
         raise Exception("Box not found?")
     canPushLeft, canPushRight = False, False
-    if warehouse[left_side[0] + dirs[m][0]][left_side[1] + dirs[m][1]] == '.':
+    next_left = left_side + dirs[m]
+    if warehouse[int(next_left.real)][int(next_left.imag)] == '.':
         canPushLeft = True
-    elif warehouse[left_side[0] + dirs[m][0]][left_side[1] + dirs[m][1]] in ['[', ']']:
-        canPushLeft = canPush(warehouse, (left_side[0] + dirs[m][0], left_side[1] + dirs[m][1]), m)
+    elif warehouse[int(next_left.real)][int(next_left.imag)] in ['[', ']']:
+        canPushLeft = canPush(warehouse, next_left, m)
         
-        
-    if warehouse[right_side[0] + dirs[m][0]][right_side[1] + dirs[m][1]] == '.':
+    next_right = right_side + dirs[m]
+    if warehouse[int(next_right.real)][int(next_right.imag)] == '.':
         canPushRight = True
-    elif warehouse[right_side[0] + dirs[m][0]][right_side[1] + dirs[m][1]] in ['[', ']']:
-        canPushRight = canPush(warehouse, (right_side[0] + dirs[m][0], right_side[1] + dirs[m][1]), m)
+    elif warehouse[int(next_right.real)][int(next_right.imag)] in ['[', ']']:
+        canPushRight = canPush(warehouse, next_right, m)
 
     return canPushLeft and canPushRight
 
@@ -85,64 +86,65 @@ def push(warehouse, next_pos, m):
     if not canPush(warehouse, next_pos, m):
         raise Exception("Trying to push a box into a wall")
         
-    if warehouse[next_pos[0]][next_pos[1]] == '[':
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '[':
         left_side = next_pos
-        right_side = (next_pos[0], next_pos[1] + 1)
-    elif warehouse[next_pos[0]][next_pos[1]] == ']':
+        right_side = next_pos + 1j
+    elif warehouse[int(next_pos.real)][int(next_pos.imag)] == ']':
         right_side = next_pos
-        left_side = (next_pos[0], next_pos[1] - 1)
+        left_side = next_pos - 1j
     else:
         raise Exception("Box not found?")
+    next_left = left_side + dirs[m]
+    if warehouse[int(next_left.real)][int(next_left.imag)] in ['[', ']']:
+        warehouse = push(warehouse, next_left, m)
+    next_right = right_side + dirs[m]
+    if warehouse[int(next_right.real)][int(next_right.imag)] in ['[', ']']:
+        warehouse = push(warehouse, next_right, m)
         
-    if warehouse[left_side[0] + dirs[m][0]][left_side[1] + dirs[m][1]] in ['[', ']']:
-        warehouse = push(warehouse, (left_side[0] + dirs[m][0], left_side[1] + dirs[m][1]), m)
-        
-    if warehouse[right_side[0] + dirs[m][0]][right_side[1] + dirs[m][1]] in ['[', ']']:
-        warehouse = push(warehouse, (right_side[0] + dirs[m][0], right_side[1] + dirs[m][1]), m)
-        
-    if (warehouse[left_side[0] + dirs[m][0]][left_side[1] + dirs[m][1]] == '.' and 
-       warehouse[right_side[0] + dirs[m][0]][right_side[1] + dirs[m][1]] == '.'):
-        warehouse[left_side[0] + dirs[m][0]][left_side[1] + dirs[m][1]] = '['
-        warehouse[right_side[0] + dirs[m][0]][right_side[1] + dirs[m][1]] = ']'
-        warehouse[left_side[0]][left_side[1]] = '.'
-        warehouse[right_side[0]][right_side[1]] = '.'
+    if (warehouse[int(next_left.real)][int(next_left.imag)] == '.' and 
+       warehouse[int(next_right.real)][int(next_right.imag)] == '.'):
+        warehouse[int(next_left.real)][int(next_left.imag)] = '['
+        warehouse[int(next_right.real)][int(next_right.imag)] = ']'
+        warehouse[int(left_side.real)][int(left_side.imag)] = '.'
+        warehouse[int(right_side.real)][int(right_side.imag)] = '.'
         
     return warehouse
 
 robot = new_robot
 warehouse = new_warehouse
 for m in movements:
-    next_pos = (robot[0] + dirs[m][0], robot[1] + dirs[m][1])
-    if warehouse[next_pos[0]][next_pos[1]] == '#':
+    next_pos = robot + dirs[m]
+    
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '#':
         continue
-    if warehouse[next_pos[0]][next_pos[1]] == '.':
-        warehouse[robot[0]][robot[1]] = '.'
-        warehouse[next_pos[0]][next_pos[1]] = '@'
+    if warehouse[int(next_pos.real)][int(next_pos.imag)] == '.':
+        warehouse[int(robot.real)][int(robot.imag)] = '.'
+        warehouse[int(next_pos.real)][int(next_pos.imag)] = '@'
         robot = next_pos
     else:
         if m in ['^', 'v']:
             if canPush(warehouse, next_pos, m):
                 warehouse = push(warehouse, next_pos, m)
-                warehouse[next_pos[0]][next_pos[1]] = '@'
-                warehouse[robot[0]][robot[1]] = '.'
+                warehouse[int(next_pos.real)][int(next_pos.imag)] = '@'
+                warehouse[int(robot.real)][int(robot.imag)] = '.'
                 robot = next_pos
         else:
             first_pos = next_pos
-            while warehouse[next_pos[0]][next_pos[1]] in ['[', ']']:
-                next_pos = (next_pos[0] + dirs[m][0], next_pos[1] + dirs[m][1])
+            while warehouse[int(next_pos.real)][int(next_pos.imag)] in ['[', ']']:
+                next_pos += dirs[m]
                 
-            if warehouse[next_pos[0]][next_pos[1]] == '.':
+            if warehouse[int(next_pos.real)][int(next_pos.imag)] == '.':
                 if m == '>':
-                    for i in range(first_pos[1] + 1, next_pos[1] + 1, 2):
-                        warehouse[next_pos[0]][i] = '['
-                        warehouse[next_pos[0]][i + 1] = ']'
+                    for i in range(int(first_pos.imag) + 1, int(next_pos.imag) + 1, 2):
+                        warehouse[int(next_pos.real)][i] = '['
+                        warehouse[int(next_pos.real)][i + 1] = ']'
                 else:
-                    for i in range(next_pos[1], first_pos[1], 2):
-                        warehouse[next_pos[0]][i] = '['
-                        warehouse[next_pos[0]][i + 1] = ']'
+                    for i in range(int(next_pos.imag), int(first_pos.imag), 2):
+                        warehouse[int(next_pos.real)][i] = '['
+                        warehouse[int(next_pos.real)][i + 1] = ']'
                 
-                warehouse[first_pos[0]][first_pos[1]] = '@'
-                warehouse[robot[0]][robot[1]] = '.'
+                warehouse[int(first_pos.real)][int(first_pos.imag)] = '@'
+                warehouse[int(robot.real)][int(robot.imag)] = '.'
                 robot = first_pos
             
 GPS = 0
