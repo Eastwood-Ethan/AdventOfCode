9730d561789e2bb64570859d7759f07609c13491
Eastwood-Ethan <ethaneastwood@dal.ca>
2024-12-18 16:13:04 -0400

    Day 6 improvement (~7s)

--- 2024/06.py
+++ 2024/06.py
@@ -23,18 +23,17 @@ d = [(-1,0), (0,1), (1,0), (0,-1)]
 facing = 0
 loop = 0
 
-def sim_guard(guard_loc, facing, visited):
-    temp_visited = visited[:]
+def sim_guard(guard_loc, facing, visited, guard_map):
     while 0 <= guard_loc[0] < r_len and 0 <= guard_loc[1] < c_len:
-        if (guard_loc[0], guard_loc[1], facing) in temp_visited:
+        if (guard_loc[0], guard_loc[1], facing) in visited:
             return 'loop'
-        temp_visited.append((guard_loc[0], guard_loc[1], facing))
-        
+        visited.add((guard_loc[0], guard_loc[1], facing))
+
         dx = guard_loc[0] + d[facing][0]
         dy = guard_loc[1] + d[facing][1]
         if 0 > dx or dx >= r_len or 0 > dy or dy >= c_len:
             break
-        
+
         while guard_map[dx][dy] == '#':
             facing = (facing + 1) % 4
             dx = guard_loc[0] + d[facing][0]
@@ -42,14 +41,25 @@ def sim_guard(guard_loc, facing, visited):
             
             if 0 > dx or dx >= r_len or 0 > dy or dy >= c_len:
                 break
-        else:
-            guard_loc = (dx,dy)
-            
-    return temp_visited
+        
+        guard_loc = (dx, dy)
 
-visited = sim_guard(guard_loc, facing, [])
-visited_part1 = []
-for e in visited:
-    if (e[0], e[1]) not in visited_part1:
-        visited_part1.append((e[0], e[1]))
+    return visited
+
+visited = sim_guard(guard_loc, facing, set(), guard_map)
+visited_part1 = set((e[0], e[1]) for e in visited)  # Create distinct positions
 print("Part 1:", len(visited_part1))
+
+loop = 0
+visited_part1.remove(guard_loc)
+
+for loc in visited_part1:
+    guard_map[loc[0]][loc[1]] = '#'
+    
+    res = sim_guard(guard_loc, facing, set(), guard_map)
+    if 'loop' in res:
+        loop += 1
+
+    guard_map[loc[0]][loc[1]] = '.'
+
+print("Part 2:", loop)
