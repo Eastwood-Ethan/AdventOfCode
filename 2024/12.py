ff9d26ff149b37fb6fb084b652c6dce1bb6d239f
Eastwood-Ethan <ethaneastwood@dal.ca>
2024-12-18 15:18:45 -0400

    Day 12 Part 2

--- 2024/12.py
+++ 2024/12.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 12 14:55:28 2024
-
-@author: ethan.eastwood
 """
 from collections import deque
 
@@ -11,23 +9,6 @@ garden = []
 for line in f.readlines():
     garden.append(list(line[:-1]))
 f.close()
-
-f = '''RRRRIICCFF
-RRRRIICCCF
-VVRRRCCFFF
-VVRCCCJFFF
-VVVVCJJCFE
-VVIVCCJJEE
-VVIIICJJEE
-MIIIIIJJEE
-MIIISIJEEE
-MMMISSJEEE'''
-garden = []
-for line in f.split('\n'):
-    garden.append(list(line))
-    
-gc = garden[:]
-
 
 r_len, c_len = len(garden), len(garden[0])
 directions = [(-1,0), (0,1), (1,0), (0,-1)]
@@ -77,48 +58,41 @@ for i in range(len(areas)):
     
 print("Part 1:", price)
 
-# region[0] is always on the edge and we can start on the top edge
-# traverse along the edge clockwise
-# every time we hit a corner, add one to the side list and change direction
-# don't update the current pos when hitting a convex corner, you want to start the next side at the same pos
-# update current pos when hitting concave corner
-# because of this we will only count a side when we have finished traversing it
-# end loop when we are back at original pos and facing the same direction as when we started
 
-# Actually this won't work in case of "lakes"
-
-# edges_list = []
-# for region in regions:
-#     start = region[0]
-#     facing = 1
-#     edges = 0
-#     started = False
-#     pos = start
-#     print(regions.index(region))
-#     while not started or not (pos == start and facing == 1):
-#         started = True
-#         # up is d-1 and down is d+1
-#         forward_straight = [pos[0] + directions[facing][0], pos[1] + directions[facing][1]]
-#         forward_up = [pos[0] + directions[facing][0] + directions[(facing - 1) % 4][0], 
-#                       pos[1] + directions[facing][1] + directions[(facing - 1) % 4][1]]
+# list copying not working, not sure why
+f = open("12.txt", 'r')
+garden = []
+for line in f.readlines():
+    garden.append(list(line[:-1]))
+f.close()
+# Number of edges is the same as the number of corners
+corners = []
+for region in regions:
+    v = garden[region[0][0]][region[0][1]]
+    corner = 0
+    for cell in region:
+        local = [[0] * 3, [0, 1, 0], [0] * 3]
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if cell[0] + i in range(r_len) and cell[1] + j in range(c_len): 
+                    local[i + 1][j + 1] = garden[cell[0] + i][cell[1] + j] == v
         
-#         if forward_up in region: #left (concave) turn
-#             pos = forward_up
-#             facing = (facing + 1) % 4
-#             edges += 1
-#             #print("left turn")
-            
-#         elif forward_straight in region: # move straight
-#             pos = forward_straight
-#             #print("straight")
-            
-#         else: #right (convex) turn
-#             facing = (facing + 1) % 4
-#             edges += 1
-#             #print("right turn")
-            
-#         print(start, pos, facing, not started or not (pos == start and facing == 1))
-#     print('\n')
-#     edges_list.append(edges)
+        # Concave and convex corners
+        corner += (((not local[0][0]) and (local[0][1] == local[1][0])) +
+                    ((not local[0][2]) and (local[0][1] == local[1][2])) +
+                    ((not local[2][0]) and (local[2][1] == local[1][0])) +
+                    ((not local[2][2]) and (local[2][1] == local[1][2])))
+        # Cells only connected via the diagonal
+        corner += ((local[0][0] and not local[0][1] and not local[1][0]) +
+                   (local[0][2] and not local[0][1] and not local[1][2]) +
+                   (local[2][0] and not local[2][1] and not local[1][0]) +
+                   (local[2][2] and not local[2][1] and not local[1][2]))
+        
+    corners.append(corner)
     
-# print(edges_list)
+price = 0
+for i in range(len(corners)):
+    price += corners[i] * areas[i]
+    
+print("Part 2:", price)
+        
