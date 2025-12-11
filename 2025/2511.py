cdf46653e8df7afb0611609a3e19b537a072be30
Eastwood-Ethan <ethaneastwood@dal.ca>
2025-12-11 12:03:56 -0400

    Day 11 adding memoization

--- 2025/2511.py
+++ 2025/2511.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Dec 11 09:30:30 2025
 """
-from collections import deque, defaultdict
+from collections import defaultdict
 f = open("2511.txt", 'r')
 data = f.read()
 f.close()
@@ -11,26 +11,22 @@ devices = defaultdict(str)
 data = data[:-1].split('\n')
 for line in data:
     devices[line.split(':')[0]] = line.split(": ")[1].split(" ")
-    
+
+memo = {}
 def numPaths(start, end):
-    paths = 0
-    queue = deque([start])
-    loop = 0
-    while queue and loop < 100000000:
-        for wire in devices[queue[0]]:
-            if wire == end:
-                paths += 1
-                loop = 0
-            else:
-                queue.append(wire)
-                loop += 1
-        queue.popleft()
-    return paths
+    if (start, end) in memo:
+        return memo[(start, end)]
+    
+    if start == end:
+        return 1
+    else:
+        memo[(start, end)] = sum(numPaths(next_device, end) for next_device in devices[start])
+        
+    return memo[(start, end)]
     
 print("Part 1:", numPaths('you', 'out'))
 
-# VERY UGLY answer for part 2, will definitely improve at some point 
-# (runtime ~90s and no way to ensure loop count is large enough without checking if answer is correct obviously)
 total = (numPaths('svr', 'fft') * numPaths('fft', 'dac') * numPaths('dac', 'out') + 
-         numPaths('svr', 'dac') * numPaths('dac', 'fft') * numPaths('fft', 'out'))
+          numPaths('svr', 'dac') * numPaths('dac', 'fft') * numPaths('fft', 'out'))
+
 print("Part 2:", total)
